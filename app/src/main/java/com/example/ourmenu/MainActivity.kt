09832e6b39677164c4abd2153dafdda8212c493f
塩谷　佳規 <>
2020-04-04 19:18:21 +0900

    4/4 19:18

--- app/src/main/java/com/example/ourmenu/MainActivity.kt
+++ app/src/main/java/com/example/ourmenu/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.ourmenu
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.view.*
 import android.view.View.inflate
 import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.core.content.res.ComplexColorCompat.inflate
@@ -17,7 +16,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerForContextMenu(menuImage)
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if (menuText.text.isNotEmpty()){
+            menuInflater.inflate(R.menu.context,menu)
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            R.id.mail -> {
+                val subject = getString(R.string.app_name)
+                val text = "${menuText.text}が食べたい！！"
+                val uri = Uri.fromParts(
+                    "mailto",
+                    "doremococo@yahoo.co.jp",
+                    null)
+                val intent = Intent(Intent.ACTION_SENDTO , uri )
+                intent.putExtra(Intent.EXTRA_SUBJECT,subject)
+                intent.putExtra(Intent.EXTRA_TEXT,text)
+                if (intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.sms -> {
+                val text = "${menuText.text}が食べたい"
+                val uri = Uri.fromParts(
+                    "smsto",
+                    "09073167519",
+                    null
+                )
+                val intent = Intent(Intent.ACTION_SENDTO , uri)
+                intent.putExtra("smsbody",text)
+                if (intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
@@ -25,42 +71,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item?.itemId) {
+        when (item.itemId) {
             R.id.homeMenu -> {
-                whatFoodImage.setImageResource(R.drawable.cooking_mama)
-                whatFood.text = ""
+                menuImage.setImageResource(R.drawable.cooking_mama)
+                menuText.text = ""
                 return true
             }
             R.id.curryUdonMenu -> {
-                whatFoodImage.setImageResource(R.drawable.curry_udon)
-                whatFood.text = getString(R.string.curry_udon_text)
+                menuImage.setImageResource(R.drawable.curry_udon)
+                menuText.text = getString(R.string.curry_udon_text)
                 return true
             }
             R.id.lemonRamenMenu -> {
-                whatFoodImage.setImageResource(R.drawable.ramen_lemon)
-                whatFood.text = getString(R.string.lemonramen_text)
+                menuImage.setImageResource(R.drawable.ramen_lemon)
+                menuText.text = getString(R.string.lemonramen_text)
                 return true
             }
             R.id.gekikaraRamenMenu ->{
-                whatFoodImage.setImageResource(R.drawable.ramen_gekikara)
-                whatFood.text = getString(R.string.gekikararamen_text)
+                menuImage.setImageResource(R.drawable.ramen_gekikara)
+                menuText.text = getString(R.string.gekikararamen_text)
                 return true
             }
             R.id.chankoNabeMenu -> {
-                whatFoodImage.setImageResource(R.drawable.nabe_chanko)
-                whatFood.text = getString(R.string.chanko_text)
+                menuImage.setImageResource(R.drawable.nabe_chanko)
+                menuText.text = getString(R.string.chanko_text)
                 return true
             }
             R.id.nabeyakiUdonMenu -> {
-                whatFoodImage.setImageResource(R.drawable.nabeyakiudon)
-                whatFood.text = getString(R.string.nabeyakiudon_text)
+                menuImage.setImageResource(R.drawable.nabeyakiudon)
+                menuText.text = getString(R.string.nabeyakiudon_text)
                 return true
             }
-            return super.onOptionsItemSelected(item) ->{
-                whatFoodImage.setImageResource(R.drawable.cooking_mama)
-                whatFood.text = getString(R.string.what_food)
-                return true
-            }
+            return super.onOptionsItemSelected(item) -> return true
         }
     }
 }
