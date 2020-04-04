fcd5738f9f1800571bc8b1273d261d0bc28eb134
塩谷　佳規 <>
2020-04-04 12:13:15 +0900

    4/4 12:13

--- app/src/main/java/com/example/ourmenu/MainActivity.kt
+++ app/src/main/java/com/example/ourmenu/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.ourmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View.inflate
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater.inflate(R.menu.main,menu)
+        return true
     }
 }
